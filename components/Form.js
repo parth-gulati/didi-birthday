0a6107942b07b4f7afb681987419393adfab109a
pga56 <parth_gulati@sfu.ca>
2024-10-12 02:45:27 -0700

    created birthday app for di

---
+++ components/Form.js
@@ -0,0 +1,85 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import { useState, useEffect } from 'react';
+import { TextField, Button } from '@material-ui/core';
+
+export default function Form() {
+
+  const [displayForm, setDisplayForm] = useState(false)
+
+  useEffect(()=>{
+    setTimeout(()=>{
+        setDisplayForm(true)
+    },1200)
+  },[])
+
+  const displayFormClass = displayForm ? styles.fadeIn : styles.fadeOut
+
+  const handleChange = (e)=>{
+    setInput(e.target.value)
+    if(error){
+      setError(false)
+      setErrorMessage('')
+    }
+  }
+
+  const [input, setInput] = useState('');
+  const [error, setError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handlePaste = (e) =>{
+    setError(true)
+    setErrorMessage("Gotta type it chigga, no cheating")
+    e.preventDefault();
+  }
+
+  const checkInput = () =>{
+    if(input==="Mera bhai parmaatma hai wah kya baat hai maja hi aa gaya tadow"){
+      setError(false);
+      window.location.href =
+        "https://www.youtube.com/watch?v=t3x-eu9R-qo&ab_channel=LAUGHCHECKPOINT";
+      return;
+    }
+    if(input.length>0 && "Mera bhai parmaatma hai wah kya baat hai maja hi aa gaya tadow".includes(input)){
+        setError(true);
+        setErrorMessage("Lazy");  
+        return;  
+    }
+    setError(true);
+    setErrorMessage("Cmon");
+  }
+
+  return (
+    <div className={styles.container}>
+      <main className={[styles.main, displayFormClass].join(' ')}>
+      <img src="didi.JPG" className={styles.circularImg}/>
+        <h1 className={styles.title2}>
+          Happy Birthday <a href="https://www.urbandictionary.com/define.php?term=Queen">Queen</a>
+        </h1>
+
+        <p className={styles.description}>
+          Enter 	&quot;Mera bhai parmaatma hai wah kya baat hai maja hi aa gaya tadow&quot; in the input box below
+        </p>
+
+        <div className={styles.grid}>
+        <TextField
+          onPaste={handlePaste}
+          className={styles.inputval}
+          fullWidth={true}
+          id="standard-helperText"
+          label={error? ":(" : ":)"}
+          error={error}
+          helperText={error? errorMessage : undefined}
+          value={input}
+          onChange={handleChange}
+        />
+        </div>
+        <div className={styles.grid}>
+        <Button title="Because Enter Key won't work" color="primary" onClick={(e)=>{checkInput()}}>Yeppers</Button>
+        
+        </div>
+      </main>
+    </div>
+  )
+}
---
+++ misc/data.js
@@ -0,0 +1,35 @@
+const data = [
+    {
+        question: "Best friend?",
+        error: "Come on bruv, you know the answer",
+        options: ["Anmol", "Tanima", "Isha", "Dipesh"],
+        answer: "Dipesh"
+    },
+    {
+        question: "Favourite cousin?",
+        error: "You know the answer",
+        options: ["Nischay Batra", "Mallika didi", "Gehna", "Akshay bhaiya"],
+        answer: "Nischay Batra"
+    },
+    {
+        question: "Mera jiju kaun banega?",
+        error: "So close",
+        options: ["S", "D", "H", "I"],
+        answer: "S"
+    },
+    {
+        question: "What is the best thing about me?",
+        error: "There are no incorrect answers here",
+        options: ["Sense of Humor", "Cute", "Smort", "Charming"],
+        answer: "All"
+    },
+    {
+        question: "What do I like the most about you?",
+        error: "No incorrect answers here either",
+        options: ["Bhakt", "Emo", "Funny", "Sort of intelligent"],
+        answer: "All"
+    },
+
+]
+
+export default data;
